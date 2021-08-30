7f320d8e541cdf1cd27dad65f9f1b7750965a027
SJ <sj.jega2@gmail.com>
2021-08-30 16:43:28 +0000

    Created a basic server using express

---
+++ express_server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+
+app.listen(PORT, () => {
+  console.log("Server is listening on port", PORT);
+});
