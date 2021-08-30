8086c8bc65e49af1ae7ba6c3d655f296751a2555
SJ <sj.jega2@gmail.com>
2021-08-30 19:51:02 +0000

    Created a new page to show only user requested short url

--- express_server.js
+++ express_server.js
@@ -20,6 +20,13 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL];
+  const templateVars = { shortURL, longURL}
+  res.render("urls_show", templateVars);
+});
+
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
