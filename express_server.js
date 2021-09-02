87070c9b6f0844c9cbe1b05fff21077c9b21a5aa
SJ <sj.jega2@gmail.com>
2021-09-02 19:39:45 +0000

    Move all helper functions into own module. Add a reademe file.

--- express_server.js
+++ express_server.js
@@ -1,34 +1,14 @@
-//##HELPER FUNCTIONS##
-const {lookupUserByEmail} = require('./helper_functions');
-//Generate a randomstring of x length
-const generateRandomString = function(stringLength) {
-  let result = '';
-  const alphaNumChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-  for (let i = 0; i < stringLength; i++) {
-    const randomNum = Math.floor(Math.random() * alphaNumChar.length);
-    result += alphaNumChar[randomNum];
-  }
-  return result;
-};
+//Importing helper functions
+const {
+  lookupUserByEmail, 
+  generateRandomString, 
+  urlsForUserID, 
+  doesUrlBelongToUser
+} = require('./helper_functions');
 
-// Returns the urls that belong to userID
-const urlsForUserID = function(userID, database) {
-  let result = {};
-  for (let shortUrlKey in database) {
-    if (userID === database[shortUrlKey].userID) {
-      result[shortUrlKey] = {
-        longURL: database[shortUrlKey].longURL        
-      }
-    }
-  }
-  return result;
-};
-//Checks if given url belongs to user
-const doesUrlBelongToUser = function(url, userID, database) {
-  const usersUrls = urlsForUserID(userID, database);
-  const userUrlsKeysArr = Object.keys(usersUrls);
-  return userUrlsKeysArr.includes(url)
-};
+
+
+
 
 //Setting up the express server
 const express = require('express');
@@ -289,7 +269,7 @@ app.post("/logout", (req, res) => {
 //##TESTS AND TEST ENPOINTS##
 app.get("/test", (req, res) => {
   console.log(JSON.stringify(usersDatabase, 0, 2));
-  // console.log(JSON.stringify(urlDatabase, 0, 2));
+  console.log(JSON.stringify(urlDatabase, 0, 2));
   res.status(400).send("Testpage");
 });
 
--- helper_functions.js
+++ helper_functions.js
@@ -1,6 +1,5 @@
 
 
-
 //Check for existing email and return user info
 const lookupUserByEmail = function(email, database) {
   for (const user in database) {
@@ -11,8 +10,42 @@ const lookupUserByEmail = function(email, database) {
   return false;
 };
 
+//Generate a randomstring of x length
+const generateRandomString = function(stringLength) {
+  let result = '';
+  const alphaNumChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  for (let i = 0; i < stringLength; i++) {
+    const randomNum = Math.floor(Math.random() * alphaNumChar.length);
+    result += alphaNumChar[randomNum];
+  }
+  return result;
+};
+
+// Returns the urls that belong to userID
+const urlsForUserID = function(userID, database) {
+  let result = {};
+  for (let shortUrlKey in database) {
+    if (userID === database[shortUrlKey].userID) {
+      result[shortUrlKey] = {
+        longURL: database[shortUrlKey].longURL        
+      }
+    }
+  }
+  return result;
+};
+
+//Checks if given url belongs to user
+const doesUrlBelongToUser = function(url, userID, database) {
+  const usersUrls = urlsForUserID(userID, database);
+  const userUrlsKeysArr = Object.keys(usersUrls);
+  return userUrlsKeysArr.includes(url)
+};
+
 
 
 module.exports = {
-  lookupUserByEmail
+  lookupUserByEmail,
+  generateRandomString,
+  urlsForUserID,
+  doesUrlBelongToUser
 }
